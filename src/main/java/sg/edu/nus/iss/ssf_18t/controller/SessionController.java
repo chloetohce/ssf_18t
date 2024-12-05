8d54dfff0f6b74c86d8f564e21cbe3620459b9a4
chloetohce <chloetohce@gmail.com>
2024-12-05 16:36:13 +0800

    Update

--- src/main/java/sg/edu/nus/iss/ssf_18t/controller/SessionController.java
+++ src/main/java/sg/edu/nus/iss/ssf_18t/controller/SessionController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 import sg.edu.nus.iss.ssf_18t.model.Session;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/sessions")
@@ -32,6 +32,7 @@ public class SessionController {
     @PostMapping("/create")
     public String postCreateForm(@ModelAttribute("session") Session s, HttpSession session, Model model) {
         List<Session> sessions = (List<Session>) session.getAttribute("sessions");
+
         System.out.println("Before: " + sessions.toString());
         sessions.add(s);
         System.out.println("After: " + ((List<Session>)session.getAttribute("sessions")).toString());
